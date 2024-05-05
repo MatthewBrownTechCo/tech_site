f376d89e68d12df2e6dc6e2f567ea81dc671230c
MatthewBrownTechCo <matthewbrowntechco@gmail.com>
2024-05-05 16:06:27 -0400

    Lots of misc changes

--- FirstProject/JavaScript/contact.js
+++ FirstProject/JavaScript/contact.js
@@ -1,3 +1,17 @@
 function contactSub() {
   alert("Your form has been submitted!  ");
 }
+
+function missingName() {
+  alert("Please fill in your name");
+}
+
+function missingEmail() {
+  alert("Please fill in your email");
+}
+
+function missingMsg() {
+  alert("Please fill in your message");
+}
+
+// Needing a function to where if more than one fields are missing, only one alert runs.
